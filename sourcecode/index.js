3bd97cbd3e902e5826d00dfb078326df22c96365
TeaTarianGit <1653072@student.hcmus.edu.vn>
2019-08-25 21:32:25 +0700

    Update index

--- sourcecode/index.js
+++ sourcecode/index.js
@@ -12,101 +12,11 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// test
-// let repoRedis = require('./repository/repoRedis')
-
-app.get('/test', async (req, res) => {
-   res.send('testing... !')
-
-   /* --------------------------------------------------------------
-                  HOW TO USE API OF MONGODB REPOSITORY 
-      -------------------------------------------------------------- */
-
-   // console.log(await repoMongo.getAllUsers())
-
-   // var bcrypt = require('bcrypt')
-   // userInfo = await repoMongo.getUserByUsername("quoctk08")
-   // var val = false
-   // if (userInfo != null) val = await bcrypt.compare("1234567", userInfo.password)
-   // console.log(val) // result : true or false
-
-   // console.log(await repoMongo.getUserByEmail("Kienquoctran08@gmail.com"))
-
-   // var bcrypt = require('bcrypt')
-   // pass = "123456"
-   // hashpass = await bcrypt.hash(pass, 12)
-   // newUser = '{"username" : "ndctran98", "password" : "' + hashpass + '", "email" : "tranndc@vng.com.vn", "displayedname" : "Nguyễn Đỗ Cát Trân"}'
-   // newUser = JSON.parse(newUser);
-   // repoMongo.addUser(newUser)
-
-   // const uuidv4 = require('uuid/v4'); // Random uuid (version 4)
-   // newGameID = uuidv4();
-   // newGame = '{"id" : "' + newGameID + '", "user_id" : "quoctk08", "guest_id" : "ndctran", "bet_points" : 0, "status" : 1}'
-   // newGame = JSON.parse(newGame);
-   // try {
-   //    repoMongo.addGame(newGame)
-   // }
-   // catch (e) {
-   //    console.log(e)
-   // }
-
-   // console.log(await repoMongo.getGameByID("ASDASD123"))
-
-   // repoMongo.updateNameOfUser("quoctk", "Trần Kiến Quốc")
-   // repoMongo.updateEmailOfUser("quoctk08", "quoctk@vng.com.vn")
-   // repoMongo.updatePasswordOfUser("quoctk08", "123321")
-   // repoMongo.updateAvatarOfUser("quoctk08", "C://.......")
-   // repoMongo.updatePointsOfUser("quoctk08", 412)
-   // repoMongo.updateWinNumOfUser("quoctk08", 41)
-   // repoMongo.updateDrawNumOfUser("quoctk08", 86)
-   // repoMongo.updateLoseNumOfUser("quoctk08", 12)
-
-   // userInfo = '{"password" : "098098", "email" : "quoctk@vng.com.vn", "display_name" : "Kiến Quốc Trần", "avatar" : "D://asdasd.....", "points" : 45, "win_num" : 12, "draw_num" : 64, "lose_num" : 21}'
-   // userInfo = JSON.parse(userInfo);
-   // repoMongo.updateUserInfo("quoctk08", userInfo)
-
-   /* --------------------------------------------------------------
-                  HOW TO USE API OF REDIS REPOSITORY 
-      -------------------------------------------------------------- */
-   
-   // result = await service.addTokenToBLJWT("SSSSS")
-   // console.log(result)
-
-   // result = await service.addTokenToBLJWT("SSSSS2")
-   // console.log(result)
-
-   // result = await service.existTokenInBLJWT("SSSSS1")
-   // console.log(result)
-
-   // result = await service.delExpiredTokenInBLJWT()
-
-   // service.updatePointsLB ("cattran", 1000000)
-   // service.updatePointsLB ("quoctk01", 800000)
-   // service.updatePointsLB ("quoctk01", 900000)
-   // service.updatePointsLB ("quoctk02", 800000)
-// service.updatePointsLB ("quoctk02", 700000)
-   // service.updatePointsLB ("quoctk08", 250000)
-   // service.updatePointsLB ("quoctk08", 220000)
-   // console.log("-------------------------------")
-   // console.log(await service.getTop6LB("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1b2N0azA4IiwiaWF0IjoxNTY2NDQwMjgxLCJleHAiOjE1NjcwNDUwODF9.cZCLddwWwNvzGiH9oX3az0Q12B78qH9piSl1tm48htA"))
-   // console.log("-------------------------------")
-   // console.log(await service.getAllTopLB("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1b2N0azA4IiwiaWF0IjoxNTY2NDQwMjgxLCJleHAiOjE1NjcwNDUwODF9.cZCLddwWwNvzGiH9oX3az0Q12B78qH9piSl1tm48htA"))
-   // console.log("-------------------------------")
-   // console.log(await service.getMyRanking("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1b2N0azA4IiwiaWF0IjoxNTY2NDQwMjgxLCJleHAiOjE1NjcwNDUwODF9.cZCLddwWwNvzGiH9oX3az0Q12B78qH9piSl1tm48htA"))
-   // console.log("-------------------------------")
-   
-   // console.log(await repoRedis.getInfoOfAllGR());
-
-   // console.log(await service.getInfoOneGameRoom("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1b2N0azA4IiwiaWF0IjoxNTY2NDQwMjgxLCJleHAiOjE1NjcwNDUwODF9.cZCLddwWwNvzGiH9oX3az0Q12B78qH9piSl1tm48htA", "GameRoom:123"))
-})
 
 
-
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// -------------------------------RESTFUL API---------------------------------
+// ---------------------------------------------------------------------------
 
 
 
